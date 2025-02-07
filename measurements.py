72c5953b2dfd049165c70b1f836a50f2018ef303
V1nc3ntG1t <vincent.hoogstraten.2003@gmail.com>
2025-02-07 12:48:08 +0100

    Ruff formatting

--- measurements.py
+++ measurements.py
@@ -1,6 +1,12 @@
-""" Hapiness Measurements """
+"""Hapiness Measurements"""
 
-def get_happiness(voter_preference: list, voting_outcome: list, preference_weights: list = None, distance_weights: list = None):
+
+def get_happiness(
+    voter_preference: list,
+    voting_outcome: list,
+    preference_weights: list = None,
+    distance_weights: list = None,
+):
     """
     Calculate the happiness of a voter given their preference and the voting outcome.
 
@@ -10,7 +16,6 @@ def get_happiness(voter_preference: list, voting_outcome: list, preference_weigh
     :param distance_weights: The weights of the distance between the preference and the outcome.
     :return: The happiness score.
     """
-
     # Set default weights
     if not preference_weights:
         preference_weights = [1] * len(voter_preference)
@@ -21,13 +26,17 @@ def get_happiness(voter_preference: list, voting_outcome: list, preference_weigh
     happiness_score = 0
     for preference_idx in range(len(voter_preference)):
         # Retrieve distance between preference and outcome
-        distance = abs(preference_idx - voting_outcome.index(voter_preference[preference_idx]))
+        distance = abs(
+            preference_idx - voting_outcome.index(voter_preference[preference_idx])
+        )
 
         # Calculate happiness score
-        happiness_score += preference_weights[preference_idx] * distance_weights[distance]
+        happiness_score += (
+            preference_weights[preference_idx] * distance_weights[distance]
+        )
 
     return happiness_score
-    
+
 
 """ Example Usage """
 
@@ -35,10 +44,18 @@ def get_happiness(voter_preference: list, voting_outcome: list, preference_weigh
 preference_weights = [1, 0, 0]
 distance_weights = [1, 0, 0]
 
-print(get_happiness(['A', 'B', 'C'], ['A', 'B', 'C'], preference_weights, distance_weights))
+print(
+    get_happiness(
+        ["A", "B", "C"], ["A", "B", "C"], preference_weights, distance_weights
+    )
+)
 
 # Bottom-1 Binary happiness measure
 preference_weights = [1, 0, 0]
 distance_weights = [0, 0, -1]
 
-print(get_happiness(['A', 'B', 'C'], ['C', 'B', 'A'], preference_weights, distance_weights))
+print(
+    get_happiness(
+        ["A", "B", "C"], ["C", "B", "A"], preference_weights, distance_weights
+    )
+)
